94486264bf29fdc3f25cd8b27ab59376e84eb74f
Elena <elena_kireeva@ymail.com>
2016-03-18 17:23:55 +0300

    New version "uncomment.py"

--- uncomment.py
+++ uncomment.py
@@ -1,70 +1,39 @@
 #for preprocessing json file
 
 import re
+import sys
 import json
 
-def getCommentsPos(text_with_comments, comments, comments_in_quotes):
-    comments_in_quotes_pos_iter = comments_in_quotes.finditer(text_with_comments)
-    comments_pos_iter           = comments.finditer(text_with_comments)
-    
-    comments_in_quotes_pos_list = [comments_in_quotes_pos for comments_in_quotes_pos in comments_in_quotes_pos_iter] 
-    comments_pos_list = [comments_pos for comments_pos in comments_pos_iter]
-    
-    correct_comments_pos_list = []
-    
-    i = 0
-    j = 0
-    
-    len_cpl   = len(comments_pos_list)
-    len_ciqpl = len(comments_in_quotes_pos_list)
-    
-    while j < len_ciqpl:
-        if comments_pos_list[i].start() < comments_in_quotes_pos_list[j].start():
-            correct_comments_pos_list.append(comments_pos_list[i])
-            i = i + 1
-        elif comments_pos_list[i].start() > comments_in_quotes_pos_list[j].start():
-            i = i + 1
-            j = j + 1
-            
-    for k in range(i, len_cpl):
-        correct_comments_pos_list.append(comments_pos_list[k])
-    
-    return correct_comments_pos_list
-    
-def deleteComments(text_with_comment, comments_pos):
-    text_without_comment = text_with_comment
-    
-    for cp in reversed(comments_pos):
-        text_without_comment = text_without_comment[0:cp.start()] + text_without_comment[cp.end():len(text_with_comment)]
-    
-    return text_without_comment
-    
-def preprocessing(slashes, slashes_in_quotes, slashes_with_stars, slashes_with_stars_in_quotes, text_with_comment):
-    
-    sc_pos = getCommentsPos(text_with_comment, slashes, slashes_in_quotes)
-    text_without_sc = deleteComments(text_with_comment, sc_pos)
-    
-    lc_pos = getCommentsPos(text_without_sc, slashes_with_stars, slashes_with_stars_in_quotes)
-    text_without_c = deleteComments(text_without_sc, lc_pos)
-    
-    return text_without_c
-    
-
-def main():
-    slashes           = re.compile(r'//.*\n')     # // * \n
-    slashes_in_quotes = re.compile(r'".*\/\/.*"') # "*//*" 
-    
-    slashes_with_stars           = re.compile(r'\/\*\s*.*\s*.*\s*\*\/') # /* * */
-    slashes_with_stars_in_quotes = re.compile(r'"[^"]*\/\*.*\*\/"\n')   # "*/* * */"\n
+CPP_PATTERN = re.compile(
+    r"""
+          (?P<comments>
+                /\*[^*]*\*+(?:[^/*][^*]*\*+)*/  # multi-line comments
+              | \s*//(?:[^\r\n])*               # single line comments
+          )
+        | (?P<code>
+                "(?:\\.|[^"\\])*"               # double quotes
+              | '(?:\\.|[^'\\])*'               # single quotes
+              | .[^/"']*                        # everything else
+          )
+    """,
+    re.VERBOSE | re.MULTILINE | re.DOTALL
+)
         
-    file_path = input()
+def main():
+    regex = CPP_PATTERN
+        
+    if(len(sys.argv) == 2):
+        f = open(sys.argv[1], "r")
+        text_with_comments = f.read()
+        f.close()
+    else:
+        text_with_comments = ""
+        for line in sys.stdin:
+            text_with_comments = text_with_comments + line
+        
+    it = regex.finditer(text_with_comments)
+    code_blocks = [m.groupdict()["code"] for m in it]
     
-    f = open(file_path, "r")
-    text_with_comments = f.read()
-    f.close()
+    print ''.join(str(cb) for cb in code_blocks if str(cb) != 'None')
     
-    text_without_comments = preprocessing(slashes, slashes_in_quotes, slashes_with_stars, slashes_with_stars_in_quotes, text_with_comments)
-    
-    print text_without_comments
-    
-main()    
+main()
